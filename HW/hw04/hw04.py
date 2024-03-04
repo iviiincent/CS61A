095b5c9b60496b022d955a2da9e3fe3422ec3338
iviiincent <iviiincent.m@outlook.com>
2024-03-04 20:11:20 +0800

    init hw04

--- HW/hw04/hw04.py
+++ HW/hw04/hw04.py
@@ -5,12 +5,12 @@ def mobile(left, right):
     """Construct a mobile from a left arm and a right arm."""
     assert is_arm(left), "left must be a arm"
     assert is_arm(right), "right must be a arm"
-    return ['mobile', left, right]
+    return ["mobile", left, right]
 
 
 def is_mobile(m):
     """Return whether m is a mobile."""
-    return type(m) == list and len(m) == 3 and m[0] == 'mobile'
+    return type(m) == list and len(m) == 3 and m[0] == "mobile"
 
 
 def left(m):
@@ -28,12 +28,12 @@ def right(m):
 def arm(length, mobile_or_planet):
     """Construct a arm: a length of rod with a mobile or planet at the end."""
     assert is_mobile(mobile_or_planet) or is_planet(mobile_or_planet)
-    return ['arm', length, mobile_or_planet]
+    return ["arm", length, mobile_or_planet]
 
 
 def is_arm(s):
     """Return whether s is a arm."""
-    return type(s) == list and len(s) == 3 and s[0] == 'arm'
+    return type(s) == list and len(s) == 3 and s[0] == "arm"
 
 
 def length(s):
@@ -56,21 +56,18 @@ def planet(size):
 
 def size(w):
     """Select the size of a planet."""
-    assert is_planet(w), 'must call size on a planet'
+    assert is_planet(w), "must call size on a planet"
     "*** YOUR CODE HERE ***"
 
 
 def is_planet(w):
     """Whether w is a planet."""
-    return type(w) == list and len(w) == 2 and w[0] == 'planet'
+    return type(w) == list and len(w) == 2 and w[0] == "planet"
 
 
 def examples():
-    t = mobile(arm(1, planet(2)),
-               arm(2, planet(1)))
-    u = mobile(arm(5, planet(1)),
-               arm(1, mobile(arm(2, planet(3)),
-                              arm(3, planet(2)))))
+    t = mobile(arm(1, planet(2)), arm(2, planet(1)))
+    u = mobile(arm(5, planet(1)), arm(1, mobile(arm(2, planet(3)), arm(3, planet(2)))))
     v = mobile(arm(4, t), arm(2, u))
     return (t, u, v)
 
@@ -214,7 +211,7 @@ def has_path(t, word):
     >>> has_path(greetings, 'hint')
     False
     """
-    assert len(word) > 0, 'no path for empty word.'
+    assert len(word) > 0, "no path for empty word."
     "*** YOUR CODE HERE ***"
 
 
@@ -247,9 +244,8 @@ def upper_bound(x):
 
 
 def str_interval(x):
-    """Return a string representation of interval x.
-    """
-    return '{0} to {1}'.format(lower_bound(x), upper_bound(x))
+    """Return a string representation of interval x."""
+    return "{0} to {1}".format(lower_bound(x), upper_bound(x))
 
 
 def add_interval(x, y):
@@ -328,10 +324,11 @@ def quadratic(x, a, b, c):
 
 # Tree ADT
 
+
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
     for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
+        assert is_tree(branch), "branches must be trees"
     return [label] + list(branches)
 
 
@@ -381,7 +378,7 @@ def print_tree(t, indent=0):
       6
         7
     """
-    print('  ' * indent + str(label(t)))
+    print("  " * indent + str(label(t)))
     for b in branches(t):
         print_tree(b, indent + 1)
 
