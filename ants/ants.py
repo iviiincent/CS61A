7f2ffde8428df606a15941a2ef06bd189b3cccb5
iviiincent <iviiincent.m@outlook.com>
2024-03-09 12:58:06 +0800

    update 09

--- ants/ants.py
+++ ants/ants.py
@@ -380,6 +380,21 @@ class BodyguardAnt(ContainerAnt):
 
 # BEGIN Problem 9
 # The TankAnt class
+class TankAnt(ContainerAnt):
+    name = "Tank"
+    food_cost = 6
+    damage = 1
+    implemented = True
+
+    def __init__(self, health=2):
+        super().__init__(health)
+
+    def action(self, gamestate):
+        super().action(gamestate)
+        for b in self.place.bees[:]:
+            b.reduce_health(self.damage)
+
+
 # END Problem 9
 
 
