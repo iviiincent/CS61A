fcacdfd3a7a06c338c6e2bbbb9a92cb502bf71fb
iviiincent <iviiincent.m@outlook.com>
2024-03-09 14:15:30 +0800

    update 10

--- ants/ants.py
+++ ants/ants.py
@@ -54,6 +54,7 @@ class Insect:
     next_id = 0  # Every insect gets a unique id number
     damage = 0
     # ADD CLASS ATTRIBUTES HERE
+    is_waterproof = False
 
     def __init__(self, health, place=None):
         """Create an Insect with a health amount and a starting PLACE."""
@@ -406,6 +407,9 @@ class Water(Place):
         its health to 0."""
         # BEGIN Problem 10
         "*** YOUR CODE HERE ***"
+        super().add_insect(insect)
+        if insect.is_waterproof is False:
+            insect.reduce_health(insect.health)
         # END Problem 10
 
 
